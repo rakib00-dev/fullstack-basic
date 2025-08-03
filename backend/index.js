234ed685fa0315d82ae15de7b369f912c2b2d05f
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-03 22:25:54 +0600

    added dist folder and serve files

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5001;
 
-app.use(express.static('../frontend/dist'));
+app.use(express.static('../frontend/dist/'));
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
