83197b2f7533f96bd9d35117683883bf93a5b77e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-03 23:51:22 +0600

    fixed the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
 
   return (
     <>
@@ -18,6 +19,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <p>{input}</p>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
