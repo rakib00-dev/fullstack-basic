50fc43d1a570ae7f29c13c577bd027b2e88a0355
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-03 23:58:53 +0600

    added api route

---
+++ backend/api/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const port = process.env.PORT || 5001;
+
+console.log(__dirname);
+
+app.use(express.static('dist'));
+
+app.get('/', (_, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
